5f25d7d9bb35fd75038c53c5f850aea238d8a6cb
Daksha Patel <dakshapatel100@gmail.com>
2018-05-14 15:27:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,9 @@ attr_accessor :name, :artist
 def initialize(name)
 @name = name
 #  sets the artist object to belong to the song
+# starts off empty to set it equal to nil
 @artist = nil
 end
 
-def self.new_by_filename
-
-end
 
 end
