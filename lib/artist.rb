df1a56a46f6d1a87c9a6af30ff01145cc3ecd82b
Daksha Patel <dakshapatel100@gmail.com>
2018-05-14 14:53:20 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,12 +8,15 @@ def initialize (name)
   @songs = []
 end
 
+@@all = []
+#for the artist, need to add an associated song
+#associated method (public method)
 def add_song(songs)
 @songs << songs
 end
 
-def save
-
+def save(name)
+  @@all << name
 end
 
 def self.find_or_create_by_name
